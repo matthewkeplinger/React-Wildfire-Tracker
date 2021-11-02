d7206b04942028f69033f7c5940ba4a7c3679f78
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-11-02 16:26:32 -0400

    Created info box to show details about incident

--- wildfire-tracker/src/components/Map.jsx
+++ wildfire-tracker/src/components/Map.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
+import LocationInfoBox from './LocationInfoBox'
 
-const Map = ({eventData, center, zoom}) => {
-    const markers = eventData.map(ev => {
-        if (ev.categories[0].id === 8){
-            return <LocationMarker lat={ev.geometries[0].coordinates[1]} 
-                                    lng={ev.geometries[0].coordinates[0]}/>
+// define constants
+const NATURAL_EVENT_WILDFIRE = 8;
+
+const Map = ({ eventData, center, zoom }) => {
+    const [locationInfo, setLocationInfo] = useState(null)
+
+    const markers = eventData.map((ev, index) => {
+        if(ev.categories[0].id === NATURAL_EVENT_WILDFIRE) {
+            return <LocationMarker key={index} lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
         }
-
+        return null
     })
     return (
         <div className = 'map'>
@@ -19,7 +24,7 @@ const Map = ({eventData, center, zoom}) => {
             >
                 {markers}
             </GoogleMapReact>
-
+            {locationInfo && <LocationInfoBox info={locationInfo} />}
         </div>
     )
 }
